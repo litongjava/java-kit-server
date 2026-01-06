fcb19567d656d22dc266b6a3c6a817ddfed14784
litongjava <litongjava@qq.com>
2026-01-06 19:55:57 +0800

    update JavaKitApp

--- src/main/java/com/litongjava/kit/JavaKitApp.java
+++ src/main/java/com/litongjava/kit/JavaKitApp.java
@@ -10,16 +10,13 @@ import com.litongjava.tio.boot.server.TioBootServer;
 public class JavaKitApp {
   public static void main(String[] args) {
     long start = System.currentTimeMillis();
-    // 1. 创建虚拟线程 Builder
     OfVirtual ofVirtual = Thread.ofVirtual();
 
-    // 2. 创建虚拟线程工厂，并指定线程名称前缀
-    ThreadFactory factory = ofVirtual.name("tio-v-", 1).factory();
+    ThreadFactory factory = ofVirtual.name("t-io-v-", 1).factory();
 
-    // 3. 获取 TioBootServer 实例并设置线程工厂
     TioBootServer server = TioBootServer.me();
-    server.setThreadFactory(factory);
-    
+    server.setWorkThreadFactory(factory);
+
     TioApplication.run(JavaKitApp.class, new KitAppConfig(), args);
     long end = System.currentTimeMillis();
     System.out.println((end - start) + "ms");
